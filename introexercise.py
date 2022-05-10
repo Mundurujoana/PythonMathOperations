db94060342f7e5b9a2a4580658801b8c940c9760
Mundurujoana <mundujoana@gmail.com>
2022-05-10 22:54:28 +0300

    first commit

---
+++ introexercise.py
@@ -0,0 +1,52 @@
+#Check the python version you are using
+# Open the python interactive shell and do the following operations. The operands are 3 and 4
+
+print(3+4) #addition(+)
+print(3*4) #subtraction(-)
+print(3-4) #multiplication(*)
+print(3%4) #modulus(%)
+print(3/4) #division(/)
+print(3//4) #floor division operator(//)
+print(3**4) #exponential(**)
+
+#Write strings on the python interactive shell. The strings are the following:
+#Your name
+#Your family name
+#Your country
+#I am enjoying 30 days of python
+
+print("Your name")
+print("Your family name")
+print("Your country")
+print("I am enjoying 30 days of python")
+
+
+#Check the data types of the following data:
+# 10
+# 9.8
+# 3.14
+# 4 - 4j
+# ['Asabeneh', 'Python', 'Finland']
+# Your name
+# Your family name
+# Your country
+
+print(type(10))
+print(type(9.5))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Your name"))
+
+
+# Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+# Find an Euclidian distance between (2, 3) and (10, 8)
+print(3) #Integer
+print(67.8) #Float
+print(4- 4j) #Complex
+print('three') #String
+print(True) #Boolean
+print((12,'era',False)) #Tuple
+print(['One','two',"three"]) #List
+print({12,34,56}) #Set
+print({"uga":"like","kye":"love"}) #
